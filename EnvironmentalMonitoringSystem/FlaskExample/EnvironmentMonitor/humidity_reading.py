0d378c0b16dd57b740001822246872e8fd5d1b98
Ace <gogamingguy@gmail.com>
2026-04-14 13:40:57 -0700

    Applied Fixes Updates Structures Remove Excess Files

--- EnvironmentalMonitoringSystem/FlaskExample/EnvironmentMonitor/humidity_reading.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from .Humidity import Humidity
-
-def read_humidity():
-    value = 0  # placeholder (or real sensor value)
-    time = datetime.now()
-    return Humidity(value, time)
